318d038a912d7ba5e5f42685fa2fbdc26838dee8
OussamaHaddi <oussama.turboxxx1@gmail.com>
2022-11-14 11:45:35 +0100

    add uml diagramme de class

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./Sass/main.scss">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YouCode clubs</title>
+</head>
+<body>
+    <h1>Test</h1>
+</body>
+</html>
