0cf3e88148ede71228c0d5b25924fb35466c5b8d
OussamaHaddi <oussama.turboxxx1@gmail.com>
2022-11-17 22:46:55 +0100

    fix some problem

--- dashboard.php
+++ dashboard.php
@@ -7,9 +7,78 @@
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800&family=IM+Fell+English+SC&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800&family=Itim&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./Sass/main.css">
-    <title>Dashboad</title>
+    <title>Dashboard</title>
 </head>
 <body>
-    
+
+    <section class="dashboard_bar">
+        <div class="dash_container">
+            <div class="logo">
+                <a href="./index.php"><img src="./Sass/images/logo.png" alt=""></a>
+            </div>
+            <div class="manage">
+                <ul>
+                    <li><img src="./Sass/images/🦆 icon _people outline_.png" alt="" width="30px"><span>Members</span></li>
+                    <li><img src="./Sass/images/🦆 icon _home outline_.png" alt="" width="30px"><span>Clubs</span></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="dashboard_header">
+        <div class="dash_container">
+            <div class="elements">
+                <div class="title">
+                    <h4><span>M</span>embers</h4>
+                </div>
+                <div class="admin_icon">
+                    <img src="./Sass/images/🦆 icon _search outline_.png" alt="">
+                    <img src="./Sass/images/🦆 icon _bell outline_.png" alt="">
+                    <img src="./Sass/images/art 1.png" alt="" width="50px" class="admin">
+                    <img src="./Sass/images/🦆 icon _log out_.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="tableaux_membre">
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Class</th>
+                    <th>Email</th>
+                    <th>Club</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td><img src="./Sass/images/art 1.png" alt="" width="50px" align="center"> <span>oussama haddi</span></td>
+                    <td>Ada lovelace</td>
+                    <td>oussama@gmail.com</td>
+                    <td>Sport</td>
+                    <td><img src="./Sass/images/more.png" alt="" width="20px"></td>
+                </tr>
+                <tr>
+                    <td><img src="./Sass/images/art 1.png" alt="" width="50px"align="center"> <span>oussama haddi</span></td>
+                    <td>Ada lovelace</td>
+                    <td>oussama@gmail.com</td>
+                    <td>Sport</td>
+                    <td><img src="./Sass/images/more.png" alt="" width="20px"></td>
+                </tr>
+                <tr>
+                    <td><img src="./Sass/images/art 1.png" alt="" width="50px"align="center"> <span>oussama haddi</span></td>
+                    <td>Ada lovelace</td>
+                    <td>oussama@gmail.com</td>
+                    <td>Sport</td>
+                    <td><img src="./Sass/images/more.png" alt="" width="20px"></td>
+                </tr>
+                <tr>
+                    <td><img src="./Sass/images/art 1.png" alt="" width="50px"align="center"> <span>oussama haddi</span></td>
+                    <td>Ada lovelace</td>
+                    <td>oussama@gmail.com</td>
+                    <td>Sport</td>
+                    <td> <div><img src="./Sass/images/🦆 icon _edit outline_.png" alt="" width="20px"><img src="./Sass/images/🦆 icon _trash 2 outline_.png" alt="" width="20px"></div> </td>
+                </tr>
+            </table>
+        </div>
+    </section>
+
 </body>
 </html>
