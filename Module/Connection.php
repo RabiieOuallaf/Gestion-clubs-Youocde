e4db15a521e0d52900f32a18e6232a8a32eebf0c
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-11-17 11:52:17 +0100

    Sorry ousama :)

---
+++ Module/Connection.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "youcodeclubs";
+
+    $conc = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
